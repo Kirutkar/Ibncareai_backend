6b12e5ac836fbc3311ccf279d13d1974e4fa1ede
KIRUTHIKA RAMALINGAM <159514346+Kirutkar@users.noreply.github.com>
2025-04-12 03:14:53 +0400

    Update app_backend.py

--- app_backend.py
+++ app_backend.py
@@ -565,10 +565,6 @@ def upload_scan():
 
 
 
-
-
-
-
 # --- Run Flask App ---
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
